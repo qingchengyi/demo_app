863765558732f7f28169abd585529b788f7db14a
Ruiqing Wang <wrq@wrq-lab.(none)>
2014-05-13 20:22:11 +0800

    Finish demo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :microposts
+  resources :users
+  get 'users/:id' => 'user#view'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
